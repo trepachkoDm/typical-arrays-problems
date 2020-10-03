66f175e60f72c6f565365245e215a93376dabd57
trepachkoDm <blokkpres@gmail.com>
2020-10-03 12:05:50 +0300

    init: start typical-array-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if (array !== undefined && array.length !== 0) {
+    return Math.min(...array);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array !== undefined && array.length !== 0) {
+    return Math.max(...array);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array !== undefined && array.length !== 0) {
+    return array.reduce((a,b) => a +b ) / array.length;
+  }
   return 0;
 }
